638420e623c2ae897488404c54373bf338d9af9a
andygeek <andy.eulogio@gmail.com>
2024-10-22 23:19:06 -0500

    feat: add editor functionality for command management
    Implement editor opening capability and create project directory structure for command file management.

--- src/utils/commandUtils.ts
+++ src/utils/commandUtils.ts
@@ -1,9 +1,30 @@
+import { spawnSync } from 'child_process';
 import * as fs from 'fs';
+import os from 'os';
 import * as path from 'path';
+
+const projectDir = path.join(os.homedir(), '.jarvissh');
+
+if (!fs.existsSync(projectDir)) {
+  fs.mkdirSync(projectDir);
+}
+
+const COMMANDS_PATH = path.join(projectDir, 'jarvissh_commands.txt');
+
+export function openEditor() {
+  const editor = process.env.EDITOR || 'nano';
+  if (!fs.existsSync(COMMANDS_PATH)) {
+    fs.writeFileSync(COMMANDS_PATH, '');
+  }
+  const result = spawnSync(editor, [COMMANDS_PATH], { stdio: 'inherit' });
+  if (result.error) {
+    console.error('Error opening the editor:', result.error);
+  }
+}
 
 export function loadCustomCommandsContent(): string {
   try {
-    const filePath = path.resolve(__dirname, '../../customCommands.json'); // Ajusta la ruta según sea necesario
+    const filePath = path.resolve(__dirname, '../../customCommands.json');
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     return fileContent;
   } catch (error) {
